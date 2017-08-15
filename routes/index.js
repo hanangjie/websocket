9a899adc989363ded7ba0db8d27096f2b1c05930
hanangjie <hanangjie@gmail.com>
2017-08-15 16:54:22 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/layout', function(req, res, next) {
-  res.render('layout');
-});
-router.get('/nsn', function(req, res, next) {
-  res.render('nsn');
+
+var list=fs.readdirSync('views');
+list.forEach(function(i,e){
+  var item=i.split('.')[0];
+  router.get('/'+item, function(req, res, next) {
+    res.render(item);
+  });
+})
+
+router.get('/loginCode', function(req, res, next) {
+  res.render('loginCode');
 });
 
 module.exports = router;
